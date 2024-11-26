b53aefe0f323d6c724206e1b85659af29300a5f4
rosch-arthur <rosch.arthur@gmail.com>
2024-11-26 16:24:45 -0300

    Fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-
+import { NextSeo } from 'next-seo';
 import { Hero } from "@/components/hero";
 import { CategoryGrid } from "@/components/category-grid";
 import { ListArticlesByCategory } from "@/components/list-articles-by-category";
@@ -6,9 +6,31 @@ import { ListArticlesByCategory } from "@/components/list-articles-by-category";
 export default function Home() {
   return (
     <>
+      <NextSeo
+        title="Oasis - Front End Development Blog"
+        description="A blog about front-end development, tutorials, best practices, and the latest trends in web development."
+        openGraph={{
+          url: 'https://teste-oasis.vercel.app/',
+          title: 'Oasis - Front End Development Blog',
+          description: 'A blog about front-end development, tutorials, best practices, and the latest trends in web development.',
+          images: [
+            {
+              url: '../assets/cover.svg',
+              alt: 'Oasis Blog Image',
+              width: 1200,
+              height: 630,
+            },
+          ],
+          site_name: 'Oasis Blog',
+        }}
+        twitter={{
+          cardType: 'summary_large_image',
+          site: '@OasisBlog',
+        }}
+      />
       <Hero />
       <CategoryGrid />
-      <ListArticlesByCategory/>
+      <ListArticlesByCategory />
     </>
   );
 }
--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  generateRobotsTxt: true,
   images: { unoptimized: true },
 };
 
