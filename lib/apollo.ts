fa4e48b0d764376804da73fd6a1324a8a0ab6541
rosch-arthur <rosch.arthur@gmail.com>
2024-11-26 15:33:28 -0300

    Fix url Api

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -2,20 +2,22 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/graphql',
+  uri: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/graphql', 
 });
 
 const authLink = setContext((_, { headers }) => {
   let token = '';
+
+ 
   if (typeof window !== 'undefined') {
     token = localStorage.getItem('token') || '';
   }
-  
+
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    }
+      authorization: token ? `Bearer ${token}` : '',
+    },
   };
 });
 
